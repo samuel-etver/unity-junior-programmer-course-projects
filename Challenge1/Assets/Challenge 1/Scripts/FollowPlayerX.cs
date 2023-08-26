b6fa4620b6d332af13c1ce1a3166ca7285198097
Samuel Etver <samuel.etver@gmail.com>
2023-08-26 23:57:06 +0500

    Challenge 1. Fix scripts

--- Challenge1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject plane;
-    private Vector3 offset;
+    public GameObject Plane;
+    public Vector3 Offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.position = Plane.transform.position + Offset;
     }
 }
--- Challenge1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Challenge1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float Speed;
     public float rotationSpeed;
-    public float verticalInput;
 
-    // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        float verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
