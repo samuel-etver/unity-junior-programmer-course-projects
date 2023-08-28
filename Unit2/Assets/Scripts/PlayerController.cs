31d32020bdb2663eab0b60176095695aeff015eb
Samuel Etver <samuel.etver@gmail.com>
2023-08-28 23:57:00 +0500

    Unit 2. Update scripts

---
+++ Unit2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    public float XRange = 10.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * Speed));
+
+        float x = transform.position.x;
+        if (x < -XRange)
+        {
+            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+        }
+        else if (x > XRange)
+        {
+            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
