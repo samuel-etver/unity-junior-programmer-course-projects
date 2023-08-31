b5c19eb81a55e79f28a3919271a5c2bbd56feb04
Samuel Etver <samuel.etver@gmail.com>
2023-08-31 23:52:48 +0500

    Unit 2. Update PlayerController script

--- Unit2/Assets/Scripts/PlayerController.cs
+++ Unit2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float Speed = 10.0f;
     public float XRange = 10.0f;
+    public float ZMax = 15.0f;
+    public float ZMin = -1.0f;
+    public int Lives { get; private set; } = 3;
+    public int Score { get; private set; } = 0;
+
     public GameObject ProjectilePrefab;
 
     void Update()
@@ -21,6 +26,20 @@ public class PlayerController : MonoBehaviour
         else if (x > XRange)
         {
             transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+        }
+
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * (verticalInput * Time.deltaTime * Speed));
+
+        float z = transform.position.z;
+        if (z < ZMin) 
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZMin);
+        }
+        else if (z > ZMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, ZMax);
         }
 
 
